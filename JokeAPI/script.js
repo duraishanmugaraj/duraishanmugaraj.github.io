d1adcd2098ec2aaee950366bd83715c3f92092fa
duraishanmugaraj <duraishanmugaraj.r@gmail.com>
2021-06-08 15:19:54 +0530

    load time

--- JokeAPI/script.js
+++ JokeAPI/script.js
@@ -1,15 +1,15 @@
-const api ="https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,political,racist&type=single";
+const api = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,political,racist&type=single";
 
-$("a").click(function(){
-newJoke();
-$('.load').attr('id', 'loading-mask')
-setTimeout(function(){ $('.load').removeAttr('id', 'loading-mask') }, 800);
+$("a").click(function () {
+  newJoke();
+  $(".load").attr("id", "loading-mask");
+  setTimeout(function () {
+    $(".load").removeAttr("id", "loading-mask");
+  }, 1200);
 });
 
-function newJoke(){
-$.getJSON(api, function(data) {
-  $("h1").text(data.joke)
-  // console.log(data)
-})
+function newJoke() {
+  $.getJSON(api, function (data) {
+    $("h1").text(data.joke);
+  });
 }
-
