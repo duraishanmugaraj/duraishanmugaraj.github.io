645b0c68dc53a98effe0df61c37fdf04bb5a3970
duraishanmugaraj <duraishanmugaraj.r@gmail.com>
2021-09-14 22:29:27 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import HashRouter from 'react-router-dom'
-ReactDOM.render(<HashRouter>
+// import HashRouter from 'react-router-dom'
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+ReactDOM.render(<Router>
     <App />
-</HashRouter>
+</Router>
     , document.getElementById('root'));
